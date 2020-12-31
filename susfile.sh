2c66eef4e6c5251a426cdd2e725ba4b7d9d5c804
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-01-01 01:09:23 +0530

    🥳 Happy new year script added

---
+++ susfile.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Config
+name='from mavn <3s'
+correction=1
+
+trap "tput reset; tput cnorm; exit" 2
+clear
+
+tput civis
+
+line=2
+middle_column=$(($(tput cols) / 2))
+middle_column=$((middle_column-1))
+color=0
+
+let new_year=$(date +'%Y')
+
+
+while true; do
+    randomValue=$((RANDOM % 10 + 1))
+    from_middle=$((middle_column-randomValue))
+    column=$((RANDOM % (randomValue) * 2 + 1 + from_middle))
+    tput setaf $color; tput bold
+
+    tput cup 1 $column
+    echo '*'
+
+    color=$(((color+1)%8))
+
+    from_middle=0 # from left column
+    for l in H A P P Y
+    do
+        tput cup $((line+1)) $((middle_column+from_middle))
+        echo $l
+        let from_middle++
+        sleep 0.01
+    done
+
+    from_middle=1
+    for l in N E W
+    do
+        tput cup $((line+2)) $((middle_column+from_middle))
+        echo $l
+        let from_middle++
+        sleep 0.01
+    done
+
+    from_middle=0
+    for l in Y E A R
+    do
+        tput cup $((line+3)) $((middle_column+from_middle))
+        echo $l
+        let from_middle++
+        sleep 0.01
+    done
+
+    spaceCount=$((middle_column/10 + correction))
+    spaces=""
+    for (( c=1; c<=$spaceCount; c++ ))
+    do
+        spaces+=" "
+    done
+
+    tput cup $((line+4)) 1
+    toilet "${spaces}$new_year" --gay -t
+
+    tput setaf $color; tput bold
+
+    tput cup 14 $column
+    echo '*'
+
+    tput cup $((line+15)) $((middle_column-(${#name}/3)))
+    echo $name
+done
